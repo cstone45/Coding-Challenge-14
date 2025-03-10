9808544a030094b02d3df24165b6e87f0580eab1
cstone45 <cstone45@usf.edu>
2025-03-09 21:16:34 -0400

    Task 2 - Support Tickets Dynamic Addition

--- cc_14.js
+++ cc_14.js
@@ -1,1 +1,24 @@
-//Task 1: See Index.html File Setup
+//Task 1:
+// See Index.html File Setup
+
+//Task 2: Adding Support Tickets Dynamically
+const ticketContainer = document.getElementById("ticketContainer");
+const ticketCard = document.createElement("div");
+ticketCard.setAttribute("id", "ticketCard");
+ticketCard.setAttribute("class", "metric-card");
+const customerName = document.createElement("h2");
+customerName.textContent = "Customer Name:"
+const issueDescription = document.createElement("p");
+issueDescription.textContent = "Customer Issue:";
+const issuePriority = document.createElement("label");
+issuePriority.textContent = "Issue Priority:"
+const resolveButton = document.createElement("button");
+resolveButton.textContent = "Remove Issue";
+resolveButton.addEventListener("click", function() {
+    ticketContainer.removeChild(ticketCard);
+});
+ticketCard.appendChild(customerName);
+ticketCard.appendChild(issueDescription);
+ticketCard.appendChild(issuePriority);
+ticketCard.appendChild(resolveButton);
+ticketContainer.appendChild(ticketCard);
